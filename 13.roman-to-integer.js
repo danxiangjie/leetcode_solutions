9cb39cbf031f63e8753d7f23960545ba00ed0329
gaussic <dzkangshu@hotmail.com>
2017-07-13 16:33:26 +0800

    [13] Roman to Integer

--- 13.roman-to-integer.js
+++ 13.roman-to-integer.js
@@ -1,3 +1,21 @@
+/*
+ * [13] Roman to Integer
+ *
+ * https://leetcode.com/problems/roman-to-integer
+ *
+ * Easy (45.38%)
+ * Total Accepted:    
+ * Total Submissions: 
+ * Testcase Example:  '"DCXXI"'
+ *
+ * Given a roman numeral, convert it to an integer.
+ * 
+ * Input is guaranteed to be within the range from 1 to 3999.
+ */
+
+// 参照 http://www.mathisfun.com/roman-numerals.html
+// 转换：前一位小于后一位，减，前一位大于后一位，加。
+
 /**
  * @param {string} s
  * @return {number}
