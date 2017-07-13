4bf292433aaf7914c08b3713fce066dd0105356a
gaussic <dzkangshu@hotmail.com>
2017-07-13 16:26:28 +0800

    21. Merge Two Sorted Lists

--- 21.merge-two-sorted-lists.py
+++ 21.merge-two-sorted-lists.py
@@ -28,4 +28,20 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
+        p = ListNode(0)
+        sorted_list = p
+        while l1 and l2:
+            if l1.val < l2.val:
+                p.next = ListNode(l1.val)
+                p = p.next
+                l1 = l1.next
+            else:
+                p.next = ListNode(l2.val)
+                p = p.next
+                l2 = l2.next
+        if l1:
+            p.next = l1
+        else:
+            p.next = l2
+        return sorted_list.next
         
