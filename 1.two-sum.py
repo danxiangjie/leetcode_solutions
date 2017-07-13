cae28eceb3528bf4a9ba94259b71b06f38e93171
gaussic <dzkangshu@hotmail.com>
2017-07-13 16:47:04 +0800

    1 Two Sum

---
+++ 1.two-sum.py
@@ -0,0 +1,45 @@
+#
+# [1] Two Sum
+#
+# https://leetcode.com/problems/two-sum
+#
+# Easy (33.86%)
+# Total Accepted:    
+# Total Submissions: 
+# Testcase Example:  '[3,2,4]\n6'
+#
+# Given an array of integers, return indices of the two numbers such that they
+# add up to a specific target.
+# 
+# You may assume that each input would have exactly one solution, and you may
+# not use the same element twice.
+# 
+# 
+# Example:
+# 
+# Given nums = [2, 7, 11, 15], target = 9,
+# 
+# Because nums[0] + nums[1] = 2 + 7 = 9,
+# return [0, 1].
+# 
+# 
+#
+
+# map 的查询是 O(1) 的， 只需要 O(n) 就可以解决这个问题。
+
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dt = {}
+        for i in range(len(nums)):
+            if target - nums[i] in dt:
+                return [dt[target-nums[i]], i]
+            dt[nums[i]] = i
+        return []
+
+# Runtime: 62ms
+        
