0b9a878f4a24df6f6db0dee380aaf2444037f075
gaussic <dzkangshu@hotmail.com>
2017-08-05 17:57:48 +0800

    62 unique paths

---
+++ 62.unique-paths.cpp
@@ -0,0 +1,43 @@
+/*
+ * [62] Unique Paths
+ *
+ * https://leetcode.com/problems/unique-paths
+ *
+ * Medium (40.96%)
+ * Total Accepted:    
+ * Total Submissions: 
+ * Testcase Example:  '1\n2'
+ *
+ * A robot is located at the top-left corner of a m x n grid (marked 'Start' in
+ * the diagram below).
+ * 
+ * The robot can only move either down or right at any point in time. The robot
+ * is trying to reach the bottom-right corner of the grid (marked 'Finish' in
+ * the diagram below).
+ * 
+ * How many possible unique paths are there?
+ * 
+ * 
+ * 
+ * Above is a 3 x 7 grid. How many possible unique paths are there?
+ * 
+ * 
+ * Note: m and n will be at most 100.
+ */
+
+inline int min(int m, int n) { return m < n ? m : n; }
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int sum = m + n - 2;
+        int minn = min(m, n) - 1;
+        double ans = 1.0;
+        for (int i = 1; i <= minn; i++)
+        {
+            ans *= (sum--);
+            ans /= i;
+        }
+        return int(ans);
+    }
+};
