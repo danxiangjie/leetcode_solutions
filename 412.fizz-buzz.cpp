cac334e5f9c10302d250574e2d675a0281106908
gaussic <dzkangshu@hotmail.com>
2017-08-18 01:59:22 +0800

    412

--- 412.fizz-buzz.cpp
+++ 412.fizz-buzz.cpp
@@ -43,6 +43,19 @@
 class Solution {
 public:
     vector<string> fizzBuzz(int n) {
-        
+        vector<string> strs;
+        for (int i = 1; i <= n; i++) {
+            string st;
+            if (i % 15 == 0)
+                st = "FizzBuzz";
+            else if (i % 3 == 0)
+                st = "Fizz";
+            else if (i % 5 == 0)
+                st = "Buzz";
+            else
+                st = to_string(i);
+            strs.push_back(st);
+        }
+        return strs;
     }
 };
